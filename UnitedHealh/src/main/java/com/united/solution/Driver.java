4779949c2f029934a0c98be6bab018286ff9a67a
maxlerer <maxlerer92@gmail.com>
2023-03-01 23:40:01 -0600

    yup

--- UnitedHealh/src/main/java/com/united/solution/Driver.java
+++ UnitedHealh/src/main/java/com/united/solution/Driver.java
@@ -3,7 +3,7 @@ package com.united.solution;
 public class Driver {
     public static void main (String [] args){
         Solution trialSolution = new Solution();
-        String testString = "ccccddeeefffff";
+        String testString = "ccccddeeeffffeeeeef";
         String result = trialSolution.removeThirdConsecutiveCharacters(testString);
         System.out.println(result);
     }
